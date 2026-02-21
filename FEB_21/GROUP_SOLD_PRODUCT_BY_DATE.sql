ff8dd2f28522427204a30a0fba3a2329f49da89e
shivam <144848496+tryChampagne@users.noreply.github.com>
2026-02-21 20:23:44 +0530

    Add SQL query to group sold products by date
    Mistake:
    1. a table might contain duplicate rows,in this case same product will be concatenated more than once.
    
    but problem asked for DISTINCT products.

---
+++ FEB_21/GROUP_SOLD_PRODUCT_BY_DATE.sql
@@ -0,0 +1,7 @@
+# Write your MySQL query statement below
+SELECT 
+    sell_date,
+    COUNT(*) AS num_sold,
+    GROUP_CONCAT(product ORDER BY product) AS products
+FROM Activities
+GROUP BY sell_date;
