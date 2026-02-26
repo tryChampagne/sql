54ac77bf0c5fb99bac1d377e54d399ffee2110f5
shivam <144848496+tryChampagne@users.noreply.github.com>
2026-02-26 17:23:40 +0530

    Delete FEB_21 directory

--- FEB_21/GROUP_SOLD_PRODUCT_BY_DATE.sql
+++
@@ -1,7 +0,0 @@
--- # Write your MySQL query statement below
-SELECT 
-    sell_date,
-    COUNT(*) AS num_sold,
-    GROUP_CONCAT(product ORDER BY product) AS products
-FROM Activities
-GROUP BY sell_date;
